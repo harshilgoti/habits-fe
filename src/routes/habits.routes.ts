40961e88237d1c62a33868100f63c154195a3bc4
Harshil Goti <hgoti@plusonex.com>
2025-05-03 18:23:52 +0530

    Refector code

--- src/routes/habits.routes.ts
+++ src/routes/habits.routes.ts
@@ -3,10 +3,7 @@ import {
   createHabit,
   habits,
   markHabitComplete,
-  unmarkHabitComplete,
   habitStatus,
-  habitStreak,
-  monthlyCompletions,
 } from "../controllers/habits.controllers";
 import { verifyJWT } from "../middlewares/auth.middleware";
 
@@ -15,13 +12,8 @@ const router = express.Router();
 router.use(verifyJWT);
 
 router.get("/", habits);
-
 router.post("/", createHabit);
-
 router.post("/:id/complete", markHabitComplete);
-router.post("/:id/uncomplete", unmarkHabitComplete);
 router.get("/:id/status", habitStatus);
-router.get("/:id/streak", habitStreak);
-router.get("/:id/monthly", monthlyCompletions);
 
 export default router;
