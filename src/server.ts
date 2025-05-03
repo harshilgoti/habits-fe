0af878e70abf0fe2f1d5040d814079b21910e42c
Harshil Goti <hgoti@plusonex.com>
2025-05-03 13:40:02 +0530

    Small changes

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ const app = express();
 
 const corsOptions: CorsOptions = {
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTION"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTION"],
   origin: "http://localhost:3000",
 };
 
